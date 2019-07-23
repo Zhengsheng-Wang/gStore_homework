9bf73b775c93c194453234b142feea2dccc03a63
ZsWong <20160731@126.com>
2019-07-23 04:30:37 -0700

    Finished!

--- gStoreSearchForeEnd/src/core/Commander.java
+++ gStoreSearchForeEnd/src/core/Commander.java
@@ -1,10 +1,44 @@
 package core;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+
 import view.*;
 
-public class Commander {
+public class Commander{
+	private GstoreConnector gConnector;
+	private FrontEnd fEnd;
+
 	static public void main(String args[]){
-		view.FrontEnd fEnd = new view.FrontEnd();
-		fEnd.init();
+		Commander commander = new Commander();
+		commander.fEnd = new view.FrontEnd(commander);
+
+		commander.fEnd.init();
+		
+		commander.gConnector = new 
+				GstoreConnector("127.0.0.1", 9000, "root", "123456");
+	}
+	
+	public void queryAndPrint(){
+		String strDBState = gConnector.show();
+
+		if(!strDBState.contains(fEnd.translator.strDBName)){
+			fEnd.setLabelWaring("database is not existing! Loading...");
+			/*
+			gConnector.build(fEnd.translator.strDBName, "/home/zswong/gStore_data/" + 
+			strDBState + ".nt");
+			gConnector.load(fEnd.translator.strDBName);
+			*/
+			fEnd.setLabelWaring("database " + fEnd.translator.strDBName
+					+ " is not existing");
+		}
+		
+		fEnd.translator.strRes = 
+				gConnector.query(fEnd.translator.strDBName, fEnd.translator.strOutputFormat, 
+				fEnd.translator.strQuery);
+		fEnd.setLabelWaring("query successed");
+		System.out.println(fEnd.translator.strRes);
 	}
 }
--- gStoreSearchForeEnd/src/view/FrontEnd.java
+++ gStoreSearchForeEnd/src/view/FrontEnd.java
@@ -1,6 +1,8 @@
 package view;
 
+import core.*;
 
+import java.awt.Dimension;
 import java.awt.FileDialog;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
@@ -15,17 +17,20 @@ import javax.swing.filechooser.FileFilter;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class FrontEnd{
-	JFrame ctlFrame = new JFrame("Carl Wong gStore Query");
+	Commander commander;
+
+	JFrame ctlFrame = new JFrame("ZsWong music gStore Query");
 	JPanel panelContent = new JPanel();
 
 	JLabel labelDBName = new JLabel("Database Name");
 	JTextField textName = new JTextField(20);
-	
 	JLabel labelQueryFile = new JLabel("Query File", JLabel.LEADING);
 	JTextField textFile = new JTextField(20);   //sparql�ļ���TextField
 	JButton buttonChooseFile = new JButton("choose a file");   //ѡ��sparql�ļ��İ�ť
@@ -33,19 +38,29 @@ public class FrontEnd{
 	JLabel labelWarning = new JLabel();     //������ʾlabel
 	JTextArea textAreaQuery = new JTextArea(6, 40); //��ʾQuery���ĵ���
 	
+	JLabel labelOutFormat = new JLabel("output format:");
+	String[] strArrOutputFormat = {"html", "json"};
+	JComboBox<String> comboBoxOutputFormat = new JComboBox<>(
+			strArrOutputFormat);
+	
 	JButton buttonConfirm = new JButton("confirm");  //ȷ�ϰ�ť
+	
+	JButton buttonSaveAsFile = new JButton("save as..");
 
 	public class Translator {
-		String strDBName;   //���ݿ������
-		String strQuery;    //��ִ�е�strQuery���
-		String strRes;     //���ص�ִ�н��
+		public String strDBName = "";   //���ݿ������
+		public String strQuery = "";    //��ִ�е�strQuery���
+		public String strOutputFormat = "";
+		public String strRes = "";     //���ص�ִ�н��
 
-		String strSparqlDir;  //Sparql �ļ���·��
-		public void translate(FrontEnd frontEnd) {
-		}
+		public String strSparqlPath = "";  //Sparql �ļ���·��
 	}
+
 	public Translator translator = new Translator();   //FrongEnd �Դ��ķ�����
 
+	public FrontEnd(Commander c){
+		commander = c;
+	}
 	//�Ű�
 	public void init(){
 		ctlFrame.setContentPane(panelContent);
@@ -93,23 +108,43 @@ public class FrontEnd{
 		
 		constraints2.gridy = 6; constraints2.gridx = 0;
 		constraints2.gridheight = 6; constraints2.gridwidth = 4;
+		textAreaQuery.setPreferredSize(new Dimension(450, 110));
 		JScrollPane scrollPane = new JScrollPane(textAreaQuery);
 		panelContent.add(scrollPane, constraints2);
-		//panelContent.add(textAreaQuery, constraints2);
 		
 		constraints2.gridy = constraints2.gridy + constraints2.gridheight; 
 		constraints2.gridx = 0;
 		constraints2.gridheight = 1; constraints2.gridwidth = 4;
 		panelContent.add(new JPanel(), constraints2);
 		
-		//����confirm��ť
 		GridBagConstraints constraints3 = new GridBagConstraints();
 		constraints3.gridy = constraints2.gridy + constraints2.gridheight;
 		constraints3.gridx = 0;
 		constraints3.gridwidth = 1;
 		constraints3.anchor = constraints3.CENTER;
 		constraints3.fill = constraints3.BOTH;
-		panelContent.add(buttonConfirm, constraints3); 
+		panelContent.add(labelOutFormat, constraints3);
+		constraints3.gridx = 1;
+		panelContent.add(comboBoxOutputFormat, constraints3);
+		translator.strOutputFormat = 
+				(String)comboBoxOutputFormat.getSelectedItem();
+		comboBoxOutputFormat.setEditable(false);
+
+		//����confirm��ť
+		GridBagConstraints constraints4 = new GridBagConstraints();
+		constraints4.gridy = constraints3.gridy + constraints3.gridheight;
+		constraints4.gridx = 0;
+		constraints4.gridwidth = 1;
+		constraints4.anchor = constraints4.CENTER;
+		constraints4.fill = constraints4.BOTH;
+		panelContent.add(buttonConfirm, constraints4); 
+
+		GridBagConstraints constraints5 = new GridBagConstraints();
+		constraints5.gridy = constraints4.gridy + constraints4.gridheight;
+		constraints5.gridx = 0;
+		constraints5.anchor = constraints5.CENTER;
+		constraints5.fill = constraints5.BOTH;
+		panelContent.add(buttonSaveAsFile, constraints5);
 
 		arrangeEvents();   //�����¼��������
 
@@ -134,7 +169,7 @@ public class FrontEnd{
 				// TODO Auto-generated method stub
 				//ѡ��sparql�ļ�
 				JFileChooser fileChooser = new JFileChooser();
-				FileNameExtensionFilter extFilter = new 
+FileNameExtensionFilter extFilter = new 
 						FileNameExtensionFilter("query files *.sql", "sql");
 				fileChooser.setFileFilter(extFilter);
 
@@ -142,7 +177,7 @@ public class FrontEnd{
 				if(iRetVal == fileChooser.APPROVE_OPTION) {
 					//���ѡȡ�ɹ������ļ��������text field��
 					File queryFile = fileChooser.getSelectedFile();
-					translator.strSparqlDir = queryFile.getAbsolutePath();
+					translator.strSparqlPath = queryFile.getAbsolutePath();
 					textFile.setText(queryFile.getName());
 					labelWarning.setText("Choosing successed!");
 					
@@ -155,7 +190,6 @@ public class FrontEnd{
 						while((strTmp = reader.readLine()) != null) {
 							strTextArea += strTmp + "\n";
 						}
-						
 						textAreaQuery.setText(strTextArea);
 					}
 					catch (IOException ioe) {
@@ -173,7 +207,6 @@ public class FrontEnd{
 				//���ļ���������������text area
 			}
 		});
-		
 		buttonConfirm.addActionListener(new ActionListener() {
 			//Ϊȷ�ϰ�ť�����¼��������
 			@Override
@@ -185,8 +218,60 @@ public class FrontEnd{
 				if(translator.strDBName.equals("") || 
 						translator.strQuery.equals("")) {
 					labelWarning.setText("database name or query text is empty!");
+					return;
+				}
+				
+				translator.strOutputFormat = 
+						(String)comboBoxOutputFormat.getSelectedItem();
+				
+				commander.queryAndPrint();
+			}
+			
+		});
+		
+		buttonSaveAsFile.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				// TODO Auto-generated method stub
+				if(translator.strRes.equals("")){
+					labelWarning.setText("no content to be saved");
+					return;
+				}
+				
+				JFileChooser fileChooser = new JFileChooser();
+				FileNameExtensionFilter extFilter = new 
+						FileNameExtensionFilter("save files *." + 
+				translator.strOutputFormat, translator.strOutputFormat);
+				fileChooser.setFileFilter(extFilter);
+				fileChooser.
+				setFileSelectionMode(fileChooser.FILES_ONLY);
+				int iRetVal = fileChooser.showSaveDialog(ctlFrame);
+				
+				if(iRetVal == fileChooser.APPROVE_OPTION){
+					File fileSave = fileChooser.getSelectedFile();
+					
+					if(!fileSave.isFile()){
+						String strName = fileChooser.getName();
+						String strDir = fileChooser.getCurrentDirectory().getAbsolutePath();
+						strName = translator.strDBName + "." + translator.strOutputFormat;
+						fileSave = new File(strDir, strName);
+					}
+
+					try{
+						BufferedWriter writer = new BufferedWriter(new FileWriter(fileSave));
+						writer.write(translator.strRes);
+						writer.close();
+					}
+					catch(IOException ioe){
+						ioe.printStackTrace();
+					}
 				}
 			}
 		});
+	}
+	
+	public void setLabelWaring(String strWarning){
+		labelWarning.setText(strWarning);
 	}
 }
